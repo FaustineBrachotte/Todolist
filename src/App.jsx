e3253be7ad82448345d69a03bf9e3d267e6ac867
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-02-26 13:55:44 +0100

    Création add to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import AddToDo from "./components/AddToDo"
 import ToDoList from "./components/ToDoList"
+import { useState } from "react"
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+
+  function addToDo(content) {
+    const toDo = {
+      id: crypto.randomUUID(),
+      content,
+      done: false,
+      edit: false
+    }
+    setToDoList([...toDoList, toDo])
+  }
 
   return (
     <div className="d-flex flex-row justify-content align-item p-20">
       <div className="card container p-20">
         <h1 className="mb-20">To do list</h1>
-        < AddToDo />
-        < ToDoList />
+        <AddToDo addToDo={addToDo}/>
+        <ToDoList />
       </div>
     </div>
   )
--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,7 +1,32 @@
-function AddToDo() {
+import { useState } from "react"
+
+// eslint-disable-next-line react/prop-types
+function AddToDo({addToDo}) {
+    const [value, setValue] = useState('')
+
+    function handleChange(e) {
+        const inputValue = e.target.value;
+        setValue(inputValue);
+    }
+
+    function handleClick() {
+        if (value.length) {
+        addToDo(value);
+        setValue('');
+        }
+    }
+
+    function handleKeyDown(e) {
+        if (e.code === 'Enter') {
+            handleClick();
+        }
+    }
 
     return (
-      <h2>Add to do</h2>
+      <div className="d-flex flex-row justify-content align-item mb-20">
+        <input className="mr-15 flex-fill" placeholder="Ajouter une to do" value={value} type="text" onChange={handleChange} onKeyDown={handleKeyDown}/>
+        <button className="btn btn-primary" onClick={handleClick}>Ajouter</button>
+      </div>
     )
   }
   
